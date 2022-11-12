ae8dc098c255e9a74c76054bacdb1d4fe10d6c12
Vincent Tran <VincentTran197120@gmail.com>
2022-11-12 13:14:04 -0600

    Fixed regression

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS  # This is the magic
 import serial
+from scipy import stats
 
 heatThreshold = 0
 naturalTempIncrease = False
@@ -8,6 +9,10 @@ currentTemperature = 0
 eta = 0
 tempHistory = []
 previousTemp = 0
+x = []
+y = []
+slope = 0
+intercept = 0
 
 app = Flask(__name__)
 
@@ -18,48 +23,45 @@ CORS(app)
 ser = serial.Serial()
 ser.baudrate = 115200
 # ser.port = 'COM8'
-ser.port = 'COM8'
+ser.port = '/dev/tty.usbserial-0001'
 
 ser.open()
 
 # -*- coding: utf-8 -*-
 #list of polled temps, approachval, size of array
-def secleft (t,a,s):
-    sumx = sumy = sumxy = sumxsq = sumysq = bz = bo = 0.0
-    for i in range(s):
-        sumx += t[i]['temp']
-        sumy += i+1
-        sumxy += t[i]['temp']*(i+1)
-        sumxsq += t[i]['temp']*t[i]['temp']
-        sumysq += (i+1)*(i+1)
-    print("a " + str(a))
-    bz = ((sumy*sumxsq) - (sumx*sumxy)) / ((s*(sumxsq)) - (sumx*sumx))
-    bo = ((s*sumxy)-(sumx*sumy)) / ((s*sumxsq) - (sumx*sumx))
-    return (bz + (a*bo)) -s 
+def secleft ():
+    r = 0
+    p = 0
+    std_err =0 
+    global slope, intercept, heatThreshold,x,y
+    slope, intercept, r, p, std_err = stats.linregress(x, y)
+    return slope*heatThreshold+intercept
 
 @app.route("/setAlarm/<threshold>/<naturalIncrease>",methods=['POST'])
 def setAlarm(threshold,naturalIncrease):
     global heatThreshold, naturalTempIncrease
     heatThreshold = float(threshold)
-    print("Inside")
-    print(heatThreshold)
     naturalTempIncrease = naturalIncrease == 0 if False else True
     return "Successfully set up alarm!"
 
 @app.route("/getInfo")
 def getThreshold():
     ser.flush()
-    global tempHistory, eta, currentTemperature, alarmActivated, heatThreshold, previousTemp
+    global tempHistory, eta, currentTemperature, alarmActivated, heatThreshold, previousTemp,x,y
     # try:
     output = ser.readline().decode("utf-8").strip().split(",")
     # print(ser.readlines())
     currentTemperature = float(output[0])
     if abs(currentTemperature - previousTemp) < 10:
         tempHistory.append({'temp': currentTemperature, 'name': output[1]})
+        x.append(float(output[1]))
+        y.append(float(currentTemperature))
     if(len(tempHistory) > 5):
         if(len(tempHistory) > 120):
             tempHistory = tempHistory[1:]
-        eta = secleft(tempHistory, heatThreshold, len(tempHistory))
+            x = x[1:]
+            y = y[1:]
+        eta = secleft()
     alarmActivated = False
     if naturalTempIncrease == True:
         alarmActivated = currentTemperature > heatThreshold 
